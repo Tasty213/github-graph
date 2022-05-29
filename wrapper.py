5f2c0e742f836cb6b917046c2a5baa938d9e586a
Tasty213 <el18gs@leeds.ac.uk>
2022-05-29 19:21:01 +0000

    Updated wrapper to accept all exceptions

--- wrapper.py
+++ wrapper.py
@@ -8,22 +8,27 @@ import logging
 # Get the logger specified in the file
 logger = logging.getLogger("mappers")
 
+
 def rate_handler(func, *args, **kwargs):
     try:
         results = func(*args, **kwargs)
     except RateLimitExceededException as limit:
         retry_time = limit.headers.get("x-ratelimit-reset")
-        if retry_time == None:
+        if retry_time is None:
             retry_time = limit.headers.get("Retry-After")
         else:
             current_time = datetime.now()
             retry_time = ceil(int(retry_time) - current_time.timestamp())
-        if retry_time == None:
-            logger.error("Rate limit hit and could not detect retry after header")
+        if retry_time is None:
+            logger.error(
+                "Rate limit hit and could not detect retry after header"
+            )
             logger.error(limit.headers)
             raise limit
         retry_time += 10
         logger.warn(f"Rate Limit hit waiting for {retry_time/60} minutes")
         sleep(retry_time)
         results = func(*args, **kwargs)
+    except Exception as e:
+        logger.warn(e)
     return results
